61582955b94b805c82e7deb69c4a25fa5b09d5ec
ankur2491 <ankur.101045@gmail.com>
2020-07-15 03:31:44 +0530

    added gcp apis

--- src/app/mapbox.service.ts
+++ src/app/mapbox.service.ts
@@ -11,14 +11,14 @@ export class MapboxService {
   constructor(private http: HttpClient) { }
 
   getWorldData(): Observable<any> {
-    return this.http.get('https://covid-service.netlify.app/.netlify/functions/api/worldData');
+    return this.http.get('http://http://35.224.154.91:5630/worldData');
   }
 
   getHistoricalData(country: string): Observable<any> {
-    return this.http.get(`https://covid-service.netlify.app/.netlify/functions/api/historicalData/${country}`);
+    return this.http.get(`http://35.224.154.91:5630/historicalData/${country}`);
   }
 
   getCountryDetails(country: string): Observable<any> {
-    return this.http.get(`https://covid-service.netlify.app/.netlify/functions/api/countryData/${country}`);
+    return this.http.get(`http://35.224.154.91:5630/countryData/${country}`);
   }
 }
